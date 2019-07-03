00fc684462f569c6f49e0213b52344600357c999
Kashmiri <kmonimahanta@gmail.com>
2019-07-04 02:58:57 +0530

    corrections made

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Recipe from './Recipe';
 
 function App() {
   const APP_ID = "3a2740fb";
-  const APP_KEY = "b1d2dce932f6e2863603451bb42df17c	—";
+  const APP_KEY = "b1d2dce932f6e2863603451bb42df17c";
   
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('banana');
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     console.log("Effect run!!");
     getRecipes();
   },[query]);
 
-  const getRecipes = async () => {
-    const response = await fetch('https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free');
-    const data = response.json();
+ var getRecipes = async () => {
+    const response = await fetch('https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}');
+    const data =await  response.json();
     setRecipes(data.hits);
-  };
+  }
 
   const updateSearch = (e) => {
     setSearch(e.target.value);
