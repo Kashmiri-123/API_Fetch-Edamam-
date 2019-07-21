f56a9d24024e5a467ed249d62ccba43e292926ea
Kashmiri-123 <kmonimahanta@gmail.com>
2019-07-21 21:24:24 +0530

    edts done

--- src/App.js
+++ src/App.js
@@ -2,46 +2,44 @@ import React,{useEffect,useState} from 'react';
 import './App.css';
 import RecipeComponent from './Recipe';
 
-function App() {
+const App=()=> {
   // const APP_ID = "3a2740fb";
   // const APP_KEY = "b1d2dce932f6e2863603451bb42df17c";
   const APP_ID = "4ece3942";
   const  APP_KEY = "14a8bc6d6966363fcb94118700646fde";
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('banana');
+  const [query, setQuery] = useState('mango');
 
   useEffect(() => {
-    console.log("Effect run!!");
     getRecipes();
   },[query]);
 
  const getRecipes = async () => {
     const response = await fetch( 
-      `https://api.edamam.com/search?q=banana&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
       );
-    const data =await response.json();
+    const data = await response.json();
     setRecipes(data.hits);
   }
 
   const updateSearch = e => {
     setSearch(e.target.value);
-  };
+  }
 
   const getSearch = e =>{
    e.preventDefault();
    setQuery(search);
    setSearch('');
-  };
+  }
 
   return (
     <div className="App">
-      <h1>Welcome</h1>
      <form onSubmit={getSearch} className="search-form">
        <input className="search-bar" type="text"  value={search} onChange={updateSearch}/>
        <button className="search-button" type="submit">Search</button>
      </form>
-     <div className="Recipe">
+     <div className="recipe">
      {recipes.map(recipe =>(
        <RecipeComponent
        label={recipe.recipe.label}
--- src/Recipe.js
+++ src/Recipe.js
@@ -12,7 +12,7 @@ const Recipe = ({title,calories,image,ingredients}) => {
                 ))}
             </ol>
             <p>{calories}</p>
-            <img className={style.image} src={image} alt=""/>
+            <img className={style.imm} src={image} alt=""/>
         </div>
     );
 }
